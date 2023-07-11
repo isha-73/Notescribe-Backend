fcfabe4c2a1ddfb2eecdacbe83cc005b9302825d
Isha Bule <ishabule753@gmail.com>
2023-07-12 00:49:23 +0530

    signin and login

--- routes/auth.js
+++ routes/auth.js
@@ -7,8 +7,9 @@ const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'thisismysecret';
 
-// Create a user using: POST "/api/auth/". Doesn't require Auth
-router.post('/', [
+// Create a user using: POST "/api/auth/createuser". Doesn't require Auth
+
+router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be at least 5 characters').isLength({ min: 5 }),
@@ -38,14 +39,13 @@ router.post('/', [
         password: secPass,
         email: req.body.email,
     })
-    const data ={
+    const data ={ // this data is the payload means the data we want to send in the token
         user:{
             id:user.id
         }
     }
     const authToken= jwt.sign(data,JWT_SECRET);
-  
-    
+
      res.json({
         user,
         authToken
@@ -55,5 +55,46 @@ router.post('/', [
     //       res.json({ error: 'Please enter a unique entry', message: err.message });
     //   });
 });
+
+// Authenticate a user using: POST "/api/auth/login". No login required
+
+router.post('/login',[body('email', 'Enter a valid email').isEmail(),
+body('password', 'Password cannot be blank').exists()]
+, async (req, res) => {
+
+  // If there are errors, return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+  }
+  const {email,password}=req.body;
+  try{
+    let user=await User.findOne({email});
+    if(!user){
+        return res.status(400).json({error:"Please try to login with correct credentials"});
+    }
+    const passwordCompare=await bcrypt.compare(password,user.password);
+    if(!passwordCompare){
+        return res.status(400).json({error:"Please try to login with correct credentials"});
+    }
+    const data ={
+        user:{
+            id:user.id
+        }
+    }
+    const authToken= jwt.sign(data,JWT_SECRET);
+    res.json({
+        authToken
+    })
+
+
+  }catch(error){
+      console.error(error.message);
+      res.status(500).send("Internal server error");
+  }
+
+});
+
+
 
 module.exports = router;
